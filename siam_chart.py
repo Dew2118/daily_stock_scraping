c429ea02bccdd763331675199eeb1277cd767241
Dew2118 <kavin2118@gmail.com>
2021-09-20 09:34:57 +0700

    test config var

--- siam_chart.py
+++ siam_chart.py
@@ -11,7 +11,9 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+# driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
 # driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get("http://siamchart.com/stock/")
 cwd = f'file:///{Path.cwd()}/Thai Stock Chart List [ข้อมูลหุ้นไทยทั้งหมด พร้อมกราฟหุ้นและเครื่องมือช่วยวิเคราะห์หุ้น และคัดกรองหุ้น] - SiamChart.html'
